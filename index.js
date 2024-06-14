ad5de7c46b2c4fdbdf072440405323efe642e6f6
Faizan Ahmad <faizanahmad2582001@gmail.com>
2024-06-14 20:56:30 +0500

    Renaming is working now

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const playerRenameButtons = document.querySelectorAll(".rename-player1-btn");
+let playerNumber = 0;
+
+const playerData = [
+  { name: "", symbol: "O" },
+  { name: "", symbol: "X" },
+];
+
+const player1RenameButtons = document.querySelector(".rename-player1-btn");
+const player2RenameButtons = document.querySelector(".rename-player2-btn");
 const playerModalCancelButton = document.querySelector(
   ".player-modal-btns button"
 );
@@ -9,7 +17,8 @@ const playerModal = document.querySelector(".player-modal");
 const playerForm = document.querySelector("form");
 const errorMessage = document.querySelector(".error-msg");
 
-function ClickToRename() {
+function ClickToRename(event) {
+  playerNumber = +event.target.dataset.player;
   backDrop.style.display = "block";
   playerModal.style.display = "block";
 }
@@ -19,24 +28,28 @@ function CancelPlayerModal() {
   errorMessage.style.display = "none";
   playerForm.children[0].classList.remove("error-label");
   playerForm.children[1].classList.remove("error-input");
+  playerForm.children[1].value = "";
 }
 function GetPlayerName(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
   const playerName = formData.get("player-name").trim();
-  console.log(playerName);
-  console.dir(event.target);
 
   if (!playerName) {
     errorMessage.style.display = "block";
     event.target.children[0].classList.add("error-label");
     event.target.children[1].classList.add("error-input");
     return;
+  } else {
+    const updatePlayerName = document.querySelector(".player-" + playerNumber);
+    updatePlayerName.children[1].textContent = playerName;
+    playerData[playerNumber - 1].name = playerName;
+    CancelPlayerModal();
   }
 }
 
-playerRenameButtons[0].addEventListener("click", ClickToRename);
-playerRenameButtons[1].addEventListener("click", ClickToRename);
+player1RenameButtons.addEventListener("click", ClickToRename);
+player2RenameButtons.addEventListener("click", ClickToRename);
 backDrop.addEventListener("click", CancelPlayerModal);
 playerModalCancelButton.addEventListener("click", CancelPlayerModal);
 playerForm.addEventListener("submit", GetPlayerName);
