24b6afcf0d04f35836c26ed1dd76afff83087116
Faizan Ahmad <faizanahmad2582001@gmail.com>
2024-06-15 16:12:20 +0500

    Game fully designed and added logic, now resetting is needed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let playerNumber = 0;
 let activePlayer = 0;
+let rounds = 1;
+let gameOver = false;
 
 const playerData = [
   { name: "", symbol: "O" },
@@ -27,6 +29,8 @@ const startGameButton = document.getElementById("start-game-btn");
 const game = document.querySelector(".game");
 const gameTile = document.querySelectorAll(".game-board li");
 const activePlayerName = document.querySelector(".game span");
+const gameStatus = document.querySelector(".game-status");
+const winnerName = document.querySelector(".winner-name");
 
 function ClickToRename(event) {
   playerNumber = +event.target.dataset.player;
@@ -61,31 +65,49 @@ function GetPlayerName(event) {
 function ClickToStartGame() {
   if (playerData[0].name === "" || playerData[1].name === "") {
     alert("Please enter Player Names first");
+    return;
+  }
+  game.style.display = "block";
+
+  activePlayerName.textContent = playerData[activePlayer].name;
+}
+
+function PlayerSwitcher() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
   } else {
-    game.style.display = "block";
+    activePlayer = 0;
   }
   activePlayerName.textContent = playerData[activePlayer].name;
 }
 
 function AddSymbol(event) {
-  activePlayerName.textContent = playerData[activePlayer].name;
+  if (gameOver === true) {
+    return;
+  }
 
   const columnTile = event.target.dataset.col - 1;
   const rowTile = event.target.dataset.row - 1;
 
   if (gameData[rowTile][columnTile] > 0) {
     alert("This field already taken, please select others which are not taken");
-  } else {
-    gameData[rowTile][columnTile] = activePlayer + 1;
-    console.log(gameData);
-    event.target.textContent = playerData[activePlayer].symbol;
-    event.target.classList.add("disabled");
-    if (activePlayer === 0) {
-      activePlayer = 1;
-    } else {
-      activePlayer = 0;
-    }
+    return;
   }
+
+  event.target.textContent = playerData[activePlayer].symbol;
+  event.target.classList.add("disabled");
+  gameData[rowTile][columnTile] = activePlayer + 1;
+  console.log(gameData);
+  let winner = GameOver();
+  console.log(winner);
+
+  if (winner !== 0) {
+    GameWinner(winner);
+  }
+
+  rounds++;
+  console.log(rounds);
+  PlayerSwitcher();
 }
 
 player1RenameButtons.addEventListener("click", ClickToRename);
@@ -97,4 +119,58 @@ startGameButton.addEventListener("click", ClickToStartGame);
 
 for (const singleTile of gameTile) {
   singleTile.addEventListener("click", AddSymbol);
+}
+
+function GameOver() {
+  for (i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
+
+  for (i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[1][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
+
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+  if (
+    gameData[2][0] > 0 &&
+    gameData[2][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[0][2]
+  ) {
+    return gameData[2][0];
+  }
+  if (rounds === 9) {
+    return -1;
+  }
+
+  return 0;
+}
+
+function GameWinner(winner) {
+  gameOver = true;
+  gameStatus.style.display = "block";
+
+  if (winner > 0) {
+    winnerName.textContent = playerData[winner - 1].name;
+  } else {
+    gameStatus.firstElementChild.firstElementChild.innerHTML =
+      '<p>It\'s a <span class="winner-name">Draw</span></p>';
+  }
 }
